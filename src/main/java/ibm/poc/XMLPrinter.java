4783c1ff8c8cd95215a3e406ddd74e000fa21815
Dmitriy Dubson <ddubson@pivotal.io>
2017-08-24 08:59:32 -0400

    Few small touchups

--- src/main/java/ibm/poc/XMLPrinter.java
+++ src/main/java/ibm/poc/XMLPrinter.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class XMLPrinter {
     public void print(Message<?> message) {
-        System.out.println("Consumed message off of queue.");
-        System.out.println("Payload: "+ message.getPayload().toString());
+        System.out.println("Consumed message off of queue.\n");
+        System.out.println("----------------------------------------------------------------");
         message.getHeaders().forEach((k,v) -> System.out.println("Header: " + k + ": " + v));
-        System.out.println("DONE.");
+        System.out.println("Payload: "+ message.getPayload().toString());
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("DONE.\n");
     }
 }
